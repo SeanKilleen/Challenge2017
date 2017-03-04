67f06b27ea396c6fc4ecf2cc7e5fc46c9896cbad
Sean Killeen <SeanKilleen@gmail.com>
2017-03-04 14:47:11 -0500

    add hello world

---
+++ src/app/Challenge2017.API/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Challenge2017.API.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult Index(IEnumerable<int> inputs)
+        {
+            var numberToGenerate = inputs.First();
+            var result = Enumerable.Repeat("Hello World", numberToGenerate);
+            return Ok(result);
+        }
+    }
+}
